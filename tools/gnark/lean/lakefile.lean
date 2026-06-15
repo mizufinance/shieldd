a80618ad22c64c0fb78eed1f6b112955bf696617
Antoine Cyr <acyrntoine@gmail.com>
2026-06-15 16:32:26 -0400

    fix

--- tools/gnark/lean/lakefile.lean
+++ tools/gnark/lean/lakefile.lean
@@ -6,5 +6,6 @@ package shieldd_gnark_formal where
 require «proven-zk» from git
   "https://github.com/reilabs/proven-zk.git" @ "4.15"
 
+@[default_target]
 lean_lib ShielddGnarkFormal where
   moreLeanArgs := #["--tstack=1000000"]
